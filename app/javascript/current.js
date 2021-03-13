30f44eae9b4feb7464bbf36688ae2f47427bf0f7
shun-moku <prmnmbr13.blue828@gmail.com>
2021-03-13 23:04:12 +0900

    再表示までのカウントダウンを実装

--- app/javascript/current.js
+++ app/javascript/current.js
@@ -13,14 +13,17 @@ function set2fig(num) {
 function showClock2() {
   var nowTime = new Date();
   var tags = JSON.parse(document.getElementById('up_time').dataset.json);
-  var count = JSON.parse(document.getElementById('count_down').dataset.json);
+  var count = JSON.parse(document.getElementById('count_down_1').dataset.json);
+  var count = JSON.parse(document.getElementById('count_down_2').dataset.json);
+
   var remain_time = Math.round((nowTime.getTime() - tags*1000)/1000)
   var nowHour = set2fig( nowTime.getHours() );
   var nowMin  = set2fig( nowTime.getMinutes() );
   var nowSec  = set2fig( nowTime.getSeconds() );
   var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
   document.getElementById("up_time").innerHTML = tags + msg + remain_time + "a" + nowTime.getTime() + "a" + tags*1000 + "a" + count;
-  document.getElementById("count_down").innerHTML = Math.round(86400 - (nowTime.getTime()/1000 -count))
+  document.getElementById("count_down_1").innerHTML = Math.round(86400 - (nowTime.getTime()/1000 -count))
+  document.getElementById("count_down_2").innerHTML = Math.round(18000 - (nowTime.getTime()/1000 -count))
   // document.getElementById("count_down").innerHTML = 86400 - (nowTime.getTime() -count*1000)
   refresh();
 }
